798b8a8ff105122df33264094167d32a6544a8a3
TienPhuc02 <dtp2352002@gmail.com>
2025-02-10 21:02:45 +0700

    setup tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3001
   },
   css: {
     devSourcemap: true
